bd14e23d534b9e8b5c8408ea30a77d5eef1b38f0
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2024-04-26 01:55:33 +0530

    update storage blog

--- routes/blog.js
+++ routes/blog.js
@@ -5,9 +5,11 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
+const uploadDir = process.env.UPLOAD_DIR;
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads`));
+    cb(null, path.resolve(`uploadDir`));
   },
   filename: function (req, file, cb) {
     const fileName = `${Date.now()}-${file.originalname}`;
